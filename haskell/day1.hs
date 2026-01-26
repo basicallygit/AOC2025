e6c882683f456e1a81cf54da5e36b3fd5a00b392
basicallygit <91993321+basicallygit@users.noreply.github.com>
2026-01-26 23:01:08 +0000

    Add files via upload

--- haskell/day1.hs
+++ haskell/day1.hs
@@ -1,30 +1,53 @@
-import Data.List (foldl')
-import Text.Read (readMaybe)
+data Lock = Lock {
+	currentDial :: Int,
+	zeroCount :: Int
+} deriving (Show)
 
-parseMove :: String -> Maybe (Char, Int)
-parseMove [] = Nothing
-parseMove (direction:distanceStr) = case readMaybe distanceStr of
-	Just distance -> Just (direction, distance)
-	Nothing       -> Nothing
+newLock :: Lock
+newLock = Lock { currentDial = 50, zeroCount = 0 }
 
-processMove :: (Int, Int) -> String -> (Int, Int)
-processMove (currentDial, zeroCount) line =
-	case parseMove line of
-		Just (direction, distance) ->
-			let
-				tempVal = case direction of
-					'L' -> (currentDial - distance) `mod` 100
-					'R' -> (currentDial + distance) `mod` 100
-					_   -> currentDial
-				newZeroCount = if tempVal == 0 then zeroCount + 1 else zeroCount
-			in (tempVal, newZeroCount)
-		Nothing -> (currentDial, zeroCount)
+incZeroCount :: Lock -> Lock
+incZeroCount lock = lock { zeroCount = zeroCount lock + 1 }
+
+lockAtZero :: Lock -> Bool
+lockAtZero lock = currentDial lock == 0
+
+doRotation :: Lock -> Int -> Lock
+doRotation lock n =
+	let
+		lock' = lock { currentDial = (currentDial lock + n + 100) `mod` 100 }
+		lock'' = if lockAtZero lock' then incZeroCount lock' else lock'
+	in
+		lock''
+
+doRotationCountAllZero :: Lock -> Int -> Lock
+doRotationCountAllZero lock n =
+	let
+		direction = if n < 0 then -1 else 1
+		steps = replicate (abs n) direction
+	in
+		foldl clickOnce lock steps
+
+clickOnce :: Lock -> Int -> Lock
+clickOnce lock d =
+	let 
+		nextDial = (currentDial lock + d + 100) `mod` 100
+		nextLock = lock { currentDial = nextDial }
+	in
+		if nextDial == 0
+		then nextLock { zeroCount = zeroCount nextLock + 1 }
+		else nextLock
+
+parseRotation :: String -> Int
+parseRotation rotationString = read $ if head rotationString == 'L' then '-':tail rotationString else tail rotationString
+
+parseLockRotations :: [String] -> [Int]
+parseLockRotations rotations = map parseRotation rotations
 
 main :: IO ()
 main = do
 	content <- readFile "input.txt"
-	let initialState = (50, 0)
-
-	let finalState = foldl' processMove initialState (lines content)
-	let (finalDial, zeroCount) = finalState
-	putStrLn $ "Num of zeroes: " ++ show zeroCount
+	let rotations = parseLockRotations (lines content)
+	--print $ zeroCount $ foldl doRotation newLock rotations
+	putStrLn $ "Part 1: " ++ show (zeroCount $ foldl doRotation newLock rotations)
+	putStrLn $ "Part 2: " ++ show (zeroCount $ foldl doRotationCountAllZero newLock rotations)
