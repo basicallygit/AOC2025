746b8fcc1e8cfa5f9263632cfbe060515392b30a
basicallygit <91993321+basicallygit@users.noreply.github.com>
2026-01-27 00:34:17 +0000

    Add part 2 for day2 in haskell

--- haskell/day2.hs
+++ haskell/day2.hs
@@ -3,13 +3,21 @@ splitOnChar splitChar str = case break (== splitChar) str of
 	(a, splitChar:b) -> a : splitOnChar splitChar b
 	(a, "")    -> [a]
 
-isValid :: Int -> Bool
-isValid n =
+isInvalid :: Int -> Bool
+isInvalid n =
 	let
 		strN = show n
 		len = length strN
 		(half1, half2) = splitAt (len `div` 2) strN
 	in even len && half1 == half2
+
+isInvalidRepeated :: Int -> Bool
+isInvalidRepeated n = any (isPattern strN) possibleLengths
+	where
+		strN = show n
+		len = length strN
+		possibleLengths = [k | k <- [1 .. len `div` 2], len `mod` k == 0]
+		isPattern str k = concat (replicate (len `div` k) (take k str)) == str
 
 parseRange :: String -> [Int]
 parseRange pair =
@@ -22,9 +30,13 @@ main = do
 	let input = head (lines content)
 	
 	let
-		ranges = splitOnChar ',' input
-		allNumbers = concatMap parseRange ranges
-		validNums  = filter isValid allNumbers
-		result     = sum validNums
+		ranges        = splitOnChar ',' input
+		allNumbers    = concatMap parseRange ranges
+		validNumsOne  = filter isInvalid allNumbers
+		resultOne     = sum validNumsOne
+		validNumsTwo  = filter isInvalidRepeated allNumbers
+		resultTwo     = sum validNumsTwo
+		
 	
-	putStrLn $ "Part 1: " ++ show result
+	putStrLn $ "Part 1: " ++ show resultOne
+	putStrLn $ "Part 2: " ++ show resultTwo
